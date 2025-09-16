a3c5bad180aa6c2703e6d6179d9614c521dc4d82
bherren <blake.herren8@gmail.com>
2025-09-15 20:27:15 -0500

    Small styling dark mode fixes + add site icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ interface HeaderProps {
 
 const Header = ({ label }: HeaderProps) => {
   return (
-    <div className="flex w-full items-center">
-      <div className="border-accent w-6 flex-shrink-0 border-t-2" />
+    <div className="dark flex w-full items-center">
+      <div className="border-muted-foreground w-6 flex-shrink-0 border-t-2" />
       <div className="mx-3 font-bold whitespace-nowrap">{label}</div>
-      <div className="border-accent flex-grow border-t-2" />
+      <div className="border-muted-foreground flex-grow border-t-2" />
     </div>
   );
 };
